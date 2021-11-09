f1d695a6feca078dbceb2049a4b67aaec19320d7
Stefan Stanciulescu <contact@stefanstanciulescu.com>
2021-11-09 19:53:03 +0100

    Port str contains command

--- crates/nu-command/src/strings/str_/mod.rs
+++ crates/nu-command/src/strings/str_/mod.rs
@@ -1,7 +1,9 @@
 mod capitalize;
 mod case;
 mod collect;
+mod contains;
 
 pub use capitalize::SubCommand as StrCapitalize;
 pub use case::*;
 pub use collect::*;
+pub use contains::SubCommand as StrContains;
