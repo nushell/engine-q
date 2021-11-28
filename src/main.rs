6ee5a9ea9600ebd57923e3a0044b390c049290e1
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2021-11-28 14:57:15 -0600

    default to on

--- src/main.rs
+++ src/main.rs
@@ -230,13 +230,15 @@ fn main() -> Result<()> {
             //Reset the ctrl-c handler
             ctrlc.store(false, Ordering::SeqCst);
 
-            // Get animation milliseconds every loop, if not set, return None to disable animation
+            // Get animation milliseconds every loop, if not set, the animation
+            // is defaulted to 1 second. If set to some non-number value,
+            // like "OFF", then the animation is disabled.
             let animate_ms = match std::env::var("EQ_PROMPT_ANIMATE_MS") {
                 Ok(ms_str) => match ms_str.parse::<u64>() {
                     Ok(ms_int) => Some(ms_int),
                     _ => None,
                 },
-                _ => None,
+                _ => Some(1000),
             };
 
             let line_editor = Reedline::create()
