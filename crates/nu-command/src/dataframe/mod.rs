fe7a5f70b6247a16883f42240777ce9c9e5514d9
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-12-05 20:34:29 +0000

    batch dataframe commands

--- crates/nu-command/src/dataframe/mod.rs
+++ crates/nu-command/src/dataframe/mod.rs
@@ -1,3 +1,25 @@
 mod nu_dataframe;
 
-pub use nu_dataframe::{DataTypes, DescribeDF, OpenDataFrame, ToDataFrame};
+use nu_dataframe::*;
+use nu_protocol::engine::StateWorkingSet;
+
+pub fn add_dataframe_decls(working_set: &mut StateWorkingSet) {
+    macro_rules! bind_command {
+            ( $command:expr ) => {
+                working_set.add_decl(Box::new($command));
+            };
+            ( $( $command:expr ),* ) => {
+                $( working_set.add_decl(Box::new($command)); )*
+            };
+        }
+
+    bind_command!(
+        AppendDF,
+        ColumnDF,
+        DataTypes,
+        DescribeDF,
+        DropDF,
+        OpenDataFrame,
+        ToDataFrame
+    );
+}
--- crates/nu-command/src/dataframe/nu_dataframe/mod.rs
+++ crates/nu-command/src/dataframe/nu_dataframe/mod.rs
@@ -1,11 +1,17 @@
-mod nu_dataframe;
+mod values;
 
+mod append;
+mod column;
 mod describe;
+mod drop;
 mod dtypes;
 mod open;
 mod to_df;
 
+pub use append::AppendDF;
+pub use column::ColumnDF;
 pub use describe::DescribeDF;
+pub use drop::DropDF;
 pub use dtypes::DataTypes;
 pub use open::OpenDataFrame;
 pub use to_df::ToDataFrame;
