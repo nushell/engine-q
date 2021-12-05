049044bf05d528eadd4c655ebc2aefa1a81ca4e4
jaeheonji <atx6419@gmail.com>
2021-12-05 19:34:49 +0900

    feat(randome): add random-integer

--- crates/nu-command/src/random/mod.rs
+++ crates/nu-command/src/random/mod.rs
@@ -3,9 +3,11 @@ mod chars;
 mod command;
 mod decimal;
 mod dice;
+mod integer;
 
 pub use self::bool::SubCommand as Bool;
 pub use self::chars::SubCommand as Chars;
 pub use self::decimal::SubCommand as Decimal;
 pub use self::dice::SubCommand as Dice;
+pub use self::integer::SubCommand as Integer;
 pub use command::RandomCommand as Random;
