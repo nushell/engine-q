7c66c3ecfa4ef13ebf4a965f955faafe156ef665
Stefan Stanciulescu <contact@stefanstanciulescu.com>
2021-12-17 20:44:40 +0100

    Final cleanup, ready for push

--- crates/nu-command/src/filters/flatten.rs
+++ crates/nu-command/src/filters/flatten.rs
@@ -4,8 +4,7 @@ use nu_protocol::ast::{Call, CellPath, PathMember};
 
 use nu_protocol::engine::{Command, EngineState, Stack};
 use nu_protocol::{
-    Category, Example,  PipelineData, ShellError,
-    Signature, Span, SyntaxShape,  Value,
+    Category, Example, PipelineData, ShellError, Signature, Span, SyntaxShape, Value,
 };
 
 #[derive(Clone)]
@@ -46,7 +45,6 @@ impl Command for Flatten {
                 description: "flatten a table",
                 example: "[[N, u, s, h, e, l, l]] | flatten ",
                 result: None
-            
             },
             Example {
                 description: "flatten a table",
@@ -75,7 +73,7 @@ fn flatten(
 ) -> Result<nu_protocol::PipelineData, nu_protocol::ShellError> {
     let tag = call.head;
     let columns: Vec<CellPath> = call.rest(engine_state, stack, 0)?;
-    
+
     input.flat_map(
         move |item| flat_value(&columns, &item, tag),
         engine_state.ctrlc.clone(),
@@ -87,43 +85,50 @@ enum TableInside<'a> {
 }
 
 fn is_table(value: &Value) -> bool {
- match value {
+    match value {
         Value::List { vals, span: _ } => vals.iter().all(|f| f.as_record().is_ok()),
         _ => false,
     }
 }
 
 fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value> {
-     
     let tag = match item.span() {
         Ok(x) => x,
-        Err(e) => return vec![Value::Error{error: e}]
+        Err(e) => return vec![Value::Error { error: e }],
     };
-        
+
     let res = {
         if item.as_record().is_ok() {
-
             let mut out = IndexMap::<String, Value>::new();
             let mut a_table = None;
             let mut tables_explicitly_flattened = 0;
 
             let records = match item {
-                Value::Record { cols, vals, span: _ } => (cols, vals),
-                x => return vec![Value::Error {error: ShellError::UnsupportedInput(format!("This should be a record, but instead got {}", x.get_type()), tag)}]
+                Value::Record {
+                    cols,
+                    vals,
+                    span: _,
+                } => (cols, vals),
+                x => {
+                    return vec![Value::Error {
+                        error: ShellError::UnsupportedInput(
+                            format!("This should be a record, but instead got {}", x.get_type()),
+                            tag,
+                        ),
+                    }]
+                }
             };
 
             let s = match item.span() {
                 Ok(x) => x,
-                Err(e) => return vec![Value::Error{error: e}]
+                Err(e) => return vec![Value::Error { error: e }],
             };
 
             for (column, value) in records.0.iter().zip(records.1.iter()) {
                 let column_requested = columns.iter().find(|c| c.into_string() == *column);
 
                 match value {
-                    Value::List { vals, span: _ }
-                        if vals.iter().all(|f| f.as_record().is_ok()) =>
-                    {
+                    Value::List { vals, span: _ } if vals.iter().all(|f| f.as_record().is_ok()) => {
                         let mut cs = vec![];
                         let mut vs = vec![];
 
@@ -135,7 +140,6 @@ fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value>
                         }
 
                         if column_requested.is_none() && !columns.is_empty() {
-                            
                             if out.contains_key(column) {
                                 out.insert(format!("{}_{}", column, column), value.clone());
                             } else {
@@ -143,7 +147,7 @@ fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value>
                             }
                             continue;
                         }
-                        
+
                         let cols = cs.into_iter().flat_map(|f| f.to_vec());
                         let vals = vs.into_iter().flat_map(|f| f.to_vec());
 
@@ -156,27 +160,25 @@ fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value>
                         }
                     }
                     Value::List { vals: _, span: _ } => {
-                     
                         let vals = if let Value::List { vals, span: _ } = value {
                             vals.iter().collect::<Vec<_>>()
                         } else {
                             vec![]
                         };
-    
+
                         if tables_explicitly_flattened >= 1 && column_requested.is_some() {
                             return vec![Value::Error{ error: ShellError::UnsupportedInput(
                                     "can only flatten one inner table at the same time. tried flattening more than one column with inner tables... but is flattened already".to_string(),
                                     s
                                 )}
-    
                             ];
                         }
-    
+
                         if !columns.is_empty() {
                             let cell_path = match column_requested {
                                 Some(x) => match x.members.first() {
-                                    Some(PathMember::String { val, span:_ }) => Some(val),
-                                    Some(PathMember::Int { val:_, span:_ }) => None,
+                                    Some(PathMember::String { val, span: _ }) => Some(val),
+                                    Some(PathMember::Int { val: _, span: _ }) => None,
                                     None => None,
                                 },
                                 None => None,
@@ -189,10 +191,9 @@ fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value>
                                         &s,
                                         vals.into_iter().collect::<Vec<_>>(),
                                     ));
-    
+
                                     tables_explicitly_flattened += 1;
                                 }
-                            
                             } else {
                                 out.insert(column.to_string(), value.clone());
                             }
@@ -203,18 +204,16 @@ fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value>
                                 vals.into_iter().collect::<Vec<_>>(),
                             ))
                         }
-                    },
-                        
+                    }
+
                     _ => {
                         out.insert(column.to_string(), value.clone());
                     }
-                    
                 }
             }
-            
 
             let mut expanded = vec![];
-            
+
             if let Some(TableInside::Entries(column, _, entries)) = a_table {
                 for entry in entries {
                     let mut base = out.clone();
@@ -227,19 +226,17 @@ fn flat_value(columns: &[CellPath], item: &Value, _name_tag: Span) -> Vec<Value>
                     expanded.push(r);
                 }
             } else {
-                
-                    let r = Value::Record {
-                        cols: out.keys().map(|f| f.to_string()).collect::<Vec<_>>(), 
-                        vals: out.values().cloned().collect(), 
-                        span: tag,
-                    };
-                    expanded.push(r);
+                let r = Value::Record {
+                    cols: out.keys().map(|f| f.to_string()).collect::<Vec<_>>(),
+                    vals: out.values().cloned().collect(),
+                    span: tag,
+                };
+                expanded.push(r);
             }
 
             expanded
-        
         } else if !is_table(item) {
-            if let Value::List { vals, span:_ } = item {
+            if let Value::List { vals, span: _ } = item {
                 vals.to_vec()
             } else {
                 vec![]
