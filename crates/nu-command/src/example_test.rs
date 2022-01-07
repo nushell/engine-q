f3257d18b2bd5c46e8fdcecb1054ff85e3698832
Jakub Žádník <kubouch@gmail.com>
2022-01-08 01:02:03 +0200

    Fix example and missing test commands

--- crates/nu-command/src/example_test.rs
+++ crates/nu-command/src/example_test.rs
@@ -13,7 +13,8 @@ use crate::To;
 
 #[cfg(test)]
 use super::{
-    Ansi, Date, From, Into, Math, Path, Random, Split, Str, StrCollect, StrFindReplace, Url,
+    Ansi, Date, From, If, Into, Math, Path, Random, Split, Str, StrCollect, StrFindReplace,
+    StrLength, Url,
 };
 
 #[cfg(test)]
@@ -29,9 +30,11 @@ pub fn test_examples(cmd: impl Command + 'static) {
         let mut working_set = StateWorkingSet::new(&*engine_state);
         working_set.add_decl(Box::new(Str));
         working_set.add_decl(Box::new(StrCollect));
+        working_set.add_decl(Box::new(StrLength));
         working_set.add_decl(Box::new(StrFindReplace));
         working_set.add_decl(Box::new(BuildString));
         working_set.add_decl(Box::new(From));
+        working_set.add_decl(Box::new(If));
         working_set.add_decl(Box::new(To));
         working_set.add_decl(Box::new(Into));
         working_set.add_decl(Box::new(Random));
