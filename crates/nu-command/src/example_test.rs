04f125d9641e49a62000ccef2a4b0467fe6a96e4
Jakub Žádník <kubouch@gmail.com>
2022-01-08 00:52:52 +0200

    Add reduce command

--- crates/nu-command/src/example_test.rs
+++ crates/nu-command/src/example_test.rs
@@ -12,7 +12,9 @@ use nu_protocol::{
 use crate::To;
 
 #[cfg(test)]
-use super::{Ansi, Date, From, Into, Math, Path, Random, Split, Str, StrCollect, Url};
+use super::{
+    Ansi, Date, From, Into, Math, Path, Random, Split, Str, StrCollect, StrFindReplace, Url,
+};
 
 #[cfg(test)]
 pub fn test_examples(cmd: impl Command + 'static) {
@@ -27,6 +29,7 @@ pub fn test_examples(cmd: impl Command + 'static) {
         let mut working_set = StateWorkingSet::new(&*engine_state);
         working_set.add_decl(Box::new(Str));
         working_set.add_decl(Box::new(StrCollect));
+        working_set.add_decl(Box::new(StrFindReplace));
         working_set.add_decl(Box::new(BuildString));
         working_set.add_decl(Box::new(From));
         working_set.add_decl(Box::new(To));
